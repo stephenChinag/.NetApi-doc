5dfd7fda1608bb27919d748ae3ca809992165cf8
stephenChinag <stephenchinag@gmail.com>
2025-03-24 21:43:17 +0100

    apidoc

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace UserRegistrationApp.Models  // Ensure this matches your project's namespace
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
